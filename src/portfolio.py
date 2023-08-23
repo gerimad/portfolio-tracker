9bd1035566821c1e457e5e96b98ae64e79dc66cb
gerimad <mgergelyjanos@gmail.com>
2023-08-23 13:58:00 +0200

    portfolio methods

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,3 +1,6 @@
+"""
+Portfolio class definition.
+"""
 
 class Portfolio:
     def __init__(self):
@@ -6,4 +9,18 @@ class Portfolio:
         self.prices = {}
     
     def add_asset(self, asset, quantity, price):
-        if asset.name
+        if asset.name not in self.assets:
+            self.assets[asset.name] = asset
+            self.quantities[asset.name] = quantity
+            self.prices[asset.name] = price
+
+    def update_price(self, asset_name, new_price):
+        if asset_name in self.prices:
+            self.prices[asset_name] = new_price
+    
+    def calculate_portfolio_value(self):
+        total_value = 0
+        for asset_name, quantity in self.quantities.items():
+            if asset_name in self.prices:
+                total_values += self.quantities[asset_name] * self.prices[asset_name]
+        return total_value

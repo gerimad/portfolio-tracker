48e79804bc8cc2ef461d1d4a67abac1710ba9cb1
gerimad <mgergelyjanos@gmail.com>
2023-08-23 14:36:06 +0200

    more portfolio tests

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,6 +1,7 @@
 """
 Portfolio class definition.
 """
+from asset import Asset
 
 class Portfolio:
     def __init__(self):
@@ -8,19 +9,19 @@ class Portfolio:
         self.quantities = {}
         self.prices = {}
     
-    def add_asset(self, asset, quantity, price):
+    def add_asset(self, asset: Asset, quantity: int, price: float) -> None:
         if asset.name not in self.assets:
             self.assets[asset.name] = asset
             self.quantities[asset.name] = quantity
             self.prices[asset.name] = price
 
-    def update_price(self, asset_name, new_price):
+    def update_price(self, asset_name: str, new_price: float) -> None:
         if asset_name in self.prices:
             self.prices[asset_name] = new_price
     
-    def calculate_portfolio_value(self):
+    def calculate_portfolio_value(self) -> float:
         total_value = 0
-        for asset_name, quantity in self.quantities.items():
+        for asset_name, _ in self.quantities.items():
             if asset_name in self.prices:
-                total_values += self.quantities[asset_name] * self.prices[asset_name]
+                total_value += self.quantities[asset_name] * self.prices[asset_name]
         return total_value
--- src/test_portfolio.py
+++ src/test_portfolio.py
@@ -19,7 +19,32 @@ class TestPortfolio(unittest.TestCase):
         self.assertEqual(self.portfolio.assets[asset1.name], asset1)
         self.assertEqual(self.portfolio.prices[asset1.name], 105.0)
         self.assertEqual(self.portfolio.quantities[asset1.name], 100)
+        # TODO test if already in, stuff doesnt change
 
+    def test_update_price(self):
+        asset1 = Asset("Apple Inc.", "Stock", "Technology", "Consumer Electronics")
+        self.portfolio.add_asset(asset1, quantity=50, price=100.0)
+        self.portfolio.update_price(asset_name=asset1.name, new_price=150)
+
+        self.assertEqual(self.portfolio.prices[asset1.name], 150)
+
+    def test_calc_portfolio(self):
+        asset1 = Asset("Apple Inc.", "Stock", "Technology", "Consumer Electronics")
+        asset2 = Asset("Microsoft Corp.", "Stock", "Technology", "Software")
+        asset3 = Asset("US Treasury Bond", "Bond", "Government", "Finance")
+        
+        self.portfolio.add_asset(asset1, quantity=50, price=100.0)
+        self.portfolio.add_asset(asset2, quantity=100, price=150.0)
+        self.portfolio.add_asset(asset3, quantity=10, price=300.0)
+
+        self.assertEqual(self.portfolio.calculate_portfolio_value(), 23000.0)
+
+
+
+
+
+
+        
 
 if __name__ == '__main__':
     unittest.main()
